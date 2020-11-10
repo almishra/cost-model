d766e68aa635ae04c9c83bc93fc470eee4c0f713
Alok Mishra <alokmishra.besu@gmail.com>
2020-11-10 08:32:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 usage()
 {
-  echo "usage: ./install [[[--prefix <PREFIX> ] [-gcc | --with-gcc]] | [-h]]"
+  echo "usage: ./install.sh [[--prefix <PREFIX> ] [-gcc | --with-gcc] [ -j | --jobs] [-h]]"
 }
 
+function MKDIR() {
+  if [ ! -d $1 ]
+  then
+    mkdir -p $1;
+  fi
+}
 
 MAKE_JOBS=4
 while [ "$1" != "" ]; do
@@ -11,6 +17,8 @@ while [ "$1" != "" ]; do
                            PREFIX="$1"
                            ;;
       -gcc | --with-gcc )  INSTALL_GCC=true
+                           ;;
+      -p | --print )       PRINT=true
                            ;;
       -j | --jobs )        shift
                            MAKE_JOBS="$1"
@@ -35,37 +43,55 @@ GCC_BUILD=$FRAMEWORK_PATH/build/gcc
 LLVM_BIN=$FRAMEWORK_PATH/opt/llvm
 GCC_BIN=$FRAMEWORK_PATH/opt/gcc
 
+if [ "$PRINT" = true ]
+then
+  echo "LLVM_SRC = $LLVM_SRC"
+  echo "GCC_SRC  = $GCC_SRC"
+  echo "LLVM_BIN = $LLVM_BIN"
+  echo "GCC_BIN  = $GCC_BIN"
+fi
+
 cd $FRAMEWORK_PATH
-mkdir src opt build
+MKDIR src
+MKDIR opt
+MKDIR build
 
 if [ "$INSTALL_GCC" = true ]
 then
-  cd $GCC_SRC
-  curl -JLO https://ftp.gnu.org/gnu/gcc/gcc-7.3.0/gcc-7.3.0.tar.xz
-  tar xf gcc-7.3.0.tar.xz
-  cd gcc-7.3.0
-  ./contrib/download_prerequisites
-  mkdir $GCC_BUILD
-  cd $GCC_BUILD
-  $GCC_SRC/gcc-7.3.0/configure \
-               --prefix=$GCC_BIN            \
-               --enable-shared              \
-               --enable-languages=c,c++,lto \
-               --enable-__cxa_atexit        \
-               --enable-threads=posix       \
-               --enable-checking=release    \
-               --disable-nls                \
-               --disable-multilib           \
-               --disable-bootstrap          \
-               --disable-libssp             \
-               --disable-libgomp            \
-               --disable-libsanitizer       \
-               --disable-libstdcxx-pch      \
-               --with-system-zlib
-  make -j $MAKE_JOBS install
-  sed -i 's/<<<ROOT>>>/$GCC_BIN/' $GCC_SRC/module/gcc-7.3.0
-  export MODULEPATH=$GCC_SRC/module:$MODULEPATH
-  module load gcc-7.3.0
+  if [ "$PRINT" = true ]
+  then
+    echo "$GCC_SRC/gcc-7.3.0/configure --prefix=$GCC_BIN --enable-shared  --enable-languages=c,c++,lto --enable-__cxa_atexit --enable-threads=posix --enable-checking=release --disable-nls --disable-multilib --disable-bootstrap --disable-libssp --disable-libgomp --disable-libsanitizer --disable-libstdcxx-pch --with-system-zlib"
+  else
+    cd $GCC_SRC
+    if [ ! -f gcc-7.3.0.tar.xz ]
+    then
+      curl -JLO https://ftp.gnu.org/gnu/gcc/gcc-7.3.0/gcc-7.3.0.tar.xz
+      tar xf gcc-7.3.0.tar.xz
+      cd gcc-7.3.0
+      ./contrib/download_prerequisites
+    fi
+    MKDIR $GCC_BUILD
+    cd $GCC_BUILD
+    $GCC_SRC/gcc-7.3.0/configure        \
+           --prefix=$GCC_BIN            \
+           --enable-shared              \
+           --enable-languages=c,c++,lto \
+           --enable-__cxa_atexit        \
+           --enable-threads=posix       \
+           --enable-checking=release    \
+           --disable-nls                \
+           --disable-multilib           \
+           --disable-bootstrap          \
+           --disable-libssp             \
+           --disable-libgomp            \
+           --disable-libsanitizer       \
+           --disable-libstdcxx-pch      \
+           --with-system-zlib
+    make -j $MAKE_JOBS && make install && \
+      sed -i "s/^set root .*/set root ${GCC_BIN//\//\\/}/" $GCC_SRC/module/gcc-7.3.0 && \
+      export MODULEPATH=$GCC_SRC/module:$MODULEPATH && \
+      module unload gcc && module load gcc-7.3.0
+  fi
   cd $FRAMEWORK_PATH
 fi
 
@@ -109,40 +135,49 @@ fi
 GCC_TOOLCHAIN=`which gcc | sed 's/\/bin\/gcc$//'`
 
 # Clone Project
-git clone --depth 1 https://github.com/llvm/llvm-project.git $LLVM_SRC
-
-if [ ! -d $CLANG_BUILD ]
-then
-  mkdir -p $CLANG_BUILD 
-fi
-if [ ! -d $OPENMP_BUILD ]
-then
-  mkdir -p $OPENMP_BUILD
+if [ "$PRINT" = true ]
+then 
+  echo "git clone --depth 1 https://github.com/llvm/llvm-project.git $LLVM_SRC"
+else
+  [[ -d $LLVM_SRC ]] || git clone --depth 1 https://github.com/llvm/llvm-project.git $LLVM_SRC
 fi
 
-echo "cmake -S $LLVM_SRC/llvm -B CLANG_BUILD -DCMAKE_INSTALL_PREFIX=$BIN -DCMAKE_BUILD_TYPE=\"Release\" -DLLVM_TARGETS_TO_BUILD=\"X86;NVPTX\" -DCMAKE_EXE_LINKER_FLAGS=\"-s\" -DCMAKE_C_COMPILER=$GCC_TOOLCHAIN/bin/gcc -DCMAKE_CXX_COMPILER=$GCC_TOOLCHAIN/bin/g++ -DGCC_INSTALL_PREFIX=$GCC_TOOLCHAIN -DLLVM_ENABLE_PROJECTS=\"clang\" -DLIBOMP_INSTALL_ALIASES=OFF -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=$ARCH -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=$CAPABILITY -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc"
-cmake -S $LLVM_SRC/llvm \
-    -B $CLANG_BUILD \
-    -DCMAKE_INSTALL_PREFIX=$BIN \
-    -DCMAKE_BUILD_TYPE="Release" \
-    -DLLVM_TARGETS_TO_BUILD="X86;NVPTX" \
-    -DCMAKE_EXE_LINKER_FLAGS="-s" \
-    -DCMAKE_C_COMPILER=$GCC_TOOLCHAIN/bin/gcc \
-    -DCMAKE_CXX_COMPILER=$GCC_TOOLCHAIN/bin/g++ \
-    -DGCC_INSTALL_PREFIX=$GCC_TOOLCHAIN \
-    -DLLVM_ENABLE_PROJECTS="clang" \
-    -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc
-make -C $CLANG_BUILD -j28 install
+if [ "$PRINT" = true ]
+then 
+  echo "cmake -S $LLVM_SRC/llvm -B $CLANG_BUILD -DCMAKE_INSTALL_PREFIX=$LLVM_BIN -DCMAKE_BUILD_TYPE=\"Release\" -DLLVM_TARGETS_TO_BUILD=\"X86;NVPTX;PowerPC\" -DCMAKE_EXE_LINKER_FLAGS=\"-s\" -DCMAKE_C_COMPILER=$GCC_TOOLCHAIN/bin/gcc -DCMAKE_CXX_COMPILER=$GCC_TOOLCHAIN/bin/g++ -DGCC_INSTALL_PREFIX=$GCC_TOOLCHAIN -DLLVM_ENABLE_PROJECTS=\"clang\" -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc"
+else
+  cmake -S $LLVM_SRC/llvm \
+        -B $CLANG_BUILD \
+        -DCMAKE_INSTALL_PREFIX=$LLVM_BIN \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;PowerPC" \
+        -DCMAKE_EXE_LINKER_FLAGS="-s" \
+        -DCMAKE_C_COMPILER=$GCC_TOOLCHAIN/bin/gcc \
+        -DCMAKE_CXX_COMPILER=$GCC_TOOLCHAIN/bin/g++ \
+        -DGCC_INSTALL_PREFIX=$GCC_TOOLCHAIN \
+        -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=$ARCH \
+        -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=$CAPABILITY \
+        -DLLVM_ENABLE_PROJECTS="clang" \
+        -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc
+  make -C $CLANG_BUILD -j $MAKE_JOBS install
+fi
 
-echo "cmake -S $LLVM_SRC/openmp -B OPENMP_BUILD -DCMAKE_INSTALL_PREFIX=$BIN -DCMAKE_BUILD_TYPE=\"Release\" -DCMAKE_EXE_LINKER_FLAGS=\"-s\" -DCMAKE_C_COMPILER=$BIN/bin/clang -DCMAKE_CXX_COMPILER=$BIN/bin/clang++ -DLIBOMP_INSTALL_ALIASES=OFF -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=$CAPABILITY -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc"
-cmake -S $LLVM_SRC/openmp \
-    -B $OPENMP_BUILD \
-    -DCMAKE_INSTALL_PREFIX=$BIN \
-    -DCMAKE_BUILD_TYPE="Release" \
-    -DCMAKE_EXE_LINKER_FLAGS="-s" \
-    -DCMAKE_C_COMPILER=$BIN/bin/clang \
-    -DCMAKE_CXX_COMPILER=$BIN/bin/clang++ \
-    -DLIBOMP_INSTALL_ALIASES=OFF \
-    -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=$CAPABILITY \
-    -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc
-make -C $OPENMP_BUILD -j28 install
+if [ "$PRINT" = true ]
+then 
+  echo "cmake -S $LLVM_SRC/openmp -B $OPENMP_BUILD -DCMAKE_INSTALL_PREFIX=$LLVM_BIN -DCMAKE_BUILD_TYPE=\"Release\" -DCMAKE_EXE_LINKER_FLAGS=\"-s\" -DLLVM_TARGETS_TO_BUILD=\"X86;NVPTX;PowerPC\" -DCMAKE_C_COMPILER=$LLVM_BIN/bin/clang -DCMAKE_CXX_COMPILER=$LLVM_BIN/bin/clang++ -DLIBOMP_INSTALL_ALIASES=OFF -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=$CAPABILITY -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc -DCMAKE_C_FLAGS=\"-I$GCC_TOOLCHAIN/include\" -DCMAKE_CXX_FLAGS=\"-stdlib++-isystem $GCC_TOOLCHAIN/include/c++/$CUR_GCC -cxx-isystem $GCC_TOOLCHAIN/include/c++/$CUR_GCC/powerpc64le-unknown-linux-gnu -I$GCC_TOOLCHAIN/include\""
+else
+  cmake -S $LLVM_SRC/openmp \
+        -B $OPENMP_BUILD \
+        -DCMAKE_INSTALL_PREFIX=$LLVM_BIN \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_EXE_LINKER_FLAGS="-s" \
+        -DCMAKE_C_COMPILER=$LLVM_BIN/bin/clang \
+        -DCMAKE_CXX_COMPILER=$LLVM_BIN/bin/clang++ \
+        -DLIBOMP_INSTALL_ALIASES=OFF \
+        -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=$ARCH \
+        -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=$CAPABILITY \
+        -DLIBOMPTARGET_NVPTX_ALTERNATE_HOST_COMPILER=$GCC_TOOLCHAIN/bin/gcc \
+        -DCMAKE_C_FLAGS="-I$GCC_TOOLCHAIN/include" \
+        -DCMAKE_CXX_FLAGS="-stdlib++-isystem $GCC_TOOLCHAIN/include/c++/$CUR_GCC -cxx-isystem $GCC_TOOLCHAIN/include/c++/$CUR_GCC/powerpc64le-unknown-linux-gnu -I$GCC_TOOLCHAIN/include" 
+  make -C $OPENMP_BUILD -j $MAKE_JOBS install
+fi
