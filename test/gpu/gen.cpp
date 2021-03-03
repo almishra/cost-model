f1ead2a13d2c6dea9eb5856ea84f863b36602da0
Alok Mishra <alokmishra.besu@gmail.com>
2021-03-02 20:21:06 -0500

    Adding gen code for GPUs

---
+++ test/gpu/gen.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <string.h>
+
+#define CODE 5
+//#define CODE 60
+
+static char code[CODE][800] = {
+  { "      for(k=0; k<LIM_1; k++) double_b[k] = 10.5;\n" },
+  { "      for(k=0; k<LIM_1; k++) double_c[k] = double_a[k] * double_b[k];\n" },
+  { "      for(k=0; k<LIM_2; k++) double_a[k] = double_c[k] / double_b[k];\n" },
+  { "      for(k=0; k<LIM_3; k++) double_b[k] = double_a[k] + double_c[k];\n" },
+  { "      for(k=0; k<LIM_1; k++) if(double_b[k] < double_a[k]) double_b[k]++;\n" },
+};
+
+void create_kernel(FILE *fp, int N)
+{
+  int num = N;
+  std::string kernel_code;
+  kernel_code.append("  for(int i=0; i<N1; i++) {\n");
+  kernel_code.append("    for(int j=0; j<N2; j++) {\n");
+  kernel_code.append("      int k;\n");
+  kernel_code.append("      double double_c[N1] = {20.0};\n");
+  int k=0;
+  while(N>0 && k<CODE) {
+    if((N&1)==1) kernel_code.append(code[k]);
+    N>>=1;
+    k++;
+  }
+  kernel_code.append("    }\n");
+  kernel_code.append("  }\n");
+  fprintf(fp, "void kernel_%d_parallel(double double_a[N1], double double_b[N1], int num_dev, int dev) {\n", num);
+  fprintf(fp, "  struct timeval t1, t2;\n");
+  fprintf(fp, "  gettimeofday(&t1, NULL);\n");
+  fprintf(fp, "#pragma omp parallel for \n");
+  fprintf(fp, "%s", kernel_code.c_str());
+  fprintf(fp, "  gettimeofday(&t2, NULL);\n");
+  fprintf(fp, "  double runtime = (t2.tv_sec - t1.tv_sec);\n");
+  fprintf(fp, "  runtime += (t2.tv_usec - t1.tv_usec) / 1000000.0;\n");
+#pragma omp critical
+  fprintf(fp, "  printf(\"kernel_%d_target,%%.3f\\n\", runtime);\n\n", num);
+  fprintf(fp, "}\n\n");
+  fprintf(fp, "void kernel_%d_target(double double_a[N1], double double_b[N1], int num_dev, int dev) {\n", num);
+  fprintf(fp, "  struct timeval t1, t2;\n");
+  fprintf(fp, "  gettimeofday(&t1, NULL);\n");
+  fprintf(fp, "#pragma omp target teams distribute parallel for map(double_a[0:N1], double_b[0:N1]) device(dev)\n");
+  fprintf(fp, "%s", kernel_code.c_str());
+  fprintf(fp, "  gettimeofday(&t2, NULL);\n");
+  fprintf(fp, "  double runtime = (t2.tv_sec - t1.tv_sec);\n");
+  fprintf(fp, "  runtime += (t2.tv_usec - t1.tv_usec) / 1000000.0;\n");
+#pragma omp critical
+  fprintf(fp, "  printf(\"kernel_%d_target,%%.3f\\n\", runtime);\n\n", num);
+  fprintf(fp, "}\n\n");
+}
+
+void create(char *filename, int N1, int N2, int LIM_1, int LIM_2, int LIM_3)
+{
+  FILE *fp = fopen(filename, "w+");
+  if(fp == NULL) {
+    fprintf(stderr, "Error: Unable to create file %s\n", filename);
+    return;
+  }
+  fprintf(fp, "#include <stdio.h>\n");
+  fprintf(fp, "#include <stdlib.h>\n");
+  fprintf(fp, "#include <omp.h>\n");
+  fprintf(fp, "#include <sys/time.h>\n\n");
+  fprintf(fp, "#define N1 %d\n", N1);
+  fprintf(fp, "#define N2 %d\n", N2);
+  fprintf(fp, "#define LIM_1 %d\n", LIM_1);
+  fprintf(fp, "#define LIM_2 %d\n", LIM_2);
+  fprintf(fp, "#define LIM_3 %d\n\n", LIM_3);
+  for(int i=0; i<1<<CODE; i++)
+    create_kernel(fp, i);
+
+  fprintf(fp, "typedef void (*Kernels) (double a[N1], double b[N1], int num_dev, int dev);\n\n");
+  fprintf(fp, "int main(int argc, char **argv)\n{\n");
+  fprintf(fp, "  if(argc < 2) {\n");
+  fprintf(fp, "    printf(\"Need Output log file name\");\n");
+  fprintf(fp, "    exit(-1);\n");
+  fprintf(fp, "  }\n");
+  fprintf(fp, "  double double_a[N1], double_b[N1];\n");
+  fprintf(fp, "  for(int i=0; i<N1; i++) {\n");
+  fprintf(fp, "    double_a[i] = 10.0;\n");
+  fprintf(fp, "    double_b[i] = 5.0;\n");
+  fprintf(fp, "  }\n\n");
+  fprintf(fp, "  Kernels kernel[] =\n");
+  fprintf(fp, "  {\n");
+  for(int i=0; i<(1<<CODE)-1; i++) {
+    fprintf(fp, "    kernel_%d_parallel,\n", i);
+    fprintf(fp, "    kernel_%d_target,\n", i);
+  }
+  fprintf(fp, "    kernel_%d_parallel,\n", (1<<CODE)-1);
+  fprintf(fp, "    kernel_%d_target\n", (1<<CODE)-1);
+  fprintf(fp, "  };\n\n");
+  fprintf(fp, "  int num_dev = omp_get_num_devices();\n");
+  fprintf(fp, "  if(num_dev > 0) {\n");
+  fprintf(fp, "    omp_set_num_threads(num_dev);\n");
+  fprintf(fp, "    for(int i=0; i<num_dev; i++)\n");
+  fprintf(fp, "#pragma omp target device(i)\n");
+  fprintf(fp, "    {}\n");
+  fprintf(fp, "  }\n");
+  fprintf(fp, "#pragma omp parallel for\n");
+  fprintf(fp, "  for(int i=0; i<1<<%d; i++)\n", CODE);
+  fprintf(fp, "    kernel[i](double_a, double_b, num_dev, omp_get_thread_num());\n");
+  fprintf(fp, "\n  return 0;\n}");
+
+  fclose(fp);
+}
+
+int main()
+{
+/*  for(int N1=100; N1<=5000; N1+=500) {
+    for(int N2=100; N2<=5000; N2+=500) {
+      for(int L1=1; L1<=N1; L1+=800) {
+        for(int L2=5; L2<=N1; L2+=325) {
+          for(int L3=9; L3<=N1; L3+=617) {*/
+  for(int N1=100; N1<=1000; N1+=500) {
+    for(int N2=100; N2<=1000; N2+=500) {
+      for(int L1=1; L1<=N1; L1+=800) {
+        for(int L2=5; L2<=N1; L2+=300) {
+          for(int L3=9; L3<=N1; L3+=300) {
+            char name[100];
+            sprintf(name, "kernel_%d_%d_%d_%d_%d.cpp", N1, N2, L1, L2, L3);
+            create(name, N1, N2, L1, L2, L3);
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
